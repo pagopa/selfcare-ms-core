eb28ff831d87dbc07ab5d7edf77f9e7335624e2b
Simone Martelli <simone.martelli@emea.nttdata.com>
2022-12-06 09:27:38 +0100

    MongoDB custom query

--- connector/dao/src/main/java/it/pagopa/selfcare/mscore/connector/dao/InstitutionRepository.java
+++ connector/dao/src/main/java/it/pagopa/selfcare/mscore/connector/dao/InstitutionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface InstitutionRepository extends MongoRepository<InstitutionEntity, ObjectId> {
+public interface InstitutionRepository extends MongoRepository<InstitutionEntity, ObjectId>, MongoCustomConnector {
 
     List<InstitutionEntity> findByExternalId(String externalId);
 
---
+++ connector/dao/src/main/java/it/pagopa/selfcare/mscore/connector/dao/MongoCustomConnectorImpl.java
@@ -0,0 +1,34 @@
+package it.pagopa.selfcare.mscore.connector.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoCustomConnectorImpl implements MongoCustomConnector {
+
+    private final MongoOperations mongoOperations;
+
+    public MongoCustomConnectorImpl(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    @Override
+    public <O> List<O> find(Query query, Class<O> outputType) {
+        return mongoOperations.find(query, outputType);
+    }
+
+    @Override
+    public <O> Page<O> find(Query query, Pageable pageable, Class<O> outputType) {
+        long count = mongoOperations.count(query, outputType);
+        List<O> list = new ArrayList<>();
+        if (count > 0) {
+            list = mongoOperations.find(query.with(pageable), outputType);
+        }
+        return new PageImpl<>(list, pageable, count);
+    }
+}
