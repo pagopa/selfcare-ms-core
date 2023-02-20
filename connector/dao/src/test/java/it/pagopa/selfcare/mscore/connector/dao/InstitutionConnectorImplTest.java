e21ffe9ae08d235d5f949edb9d2e886186ae9e98
BaldiVi <vittorio.baldi@emea.nttdata.com>
2023-02-20 17:54:19 +0100

    Test connector dao

--- connector/dao/src/test/java/it/pagopa/selfcare/mscore/connector/dao/InstitutionConnectorImplTest.java
+++ connector/dao/src/test/java/it/pagopa/selfcare/mscore/connector/dao/InstitutionConnectorImplTest.java
@@ -1,32 +1,35 @@
 package it.pagopa.selfcare.mscore.connector.dao;
 
 import it.pagopa.selfcare.mscore.connector.dao.model.InstitutionEntity;
+import it.pagopa.selfcare.mscore.exception.ResourceNotFoundException;
 import it.pagopa.selfcare.mscore.model.institution.DataProtectionOfficer;
 import it.pagopa.selfcare.mscore.model.institution.Institution;
+import it.pagopa.selfcare.mscore.model.institution.Onboarding;
 import it.pagopa.selfcare.mscore.model.institution.PaymentServiceProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Example;
-import org.springframework.test.context.ContextConfiguration;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
-@ContextConfiguration(classes = InstitutionConnectorImpl.class)
 @ExtendWith(SpringExtension.class)
 class InstitutionConnectorImplTest {
 
-    @Autowired
+    @InjectMocks
     InstitutionConnectorImpl institutionConnectionImpl;
 
-    @MockBean
+    @Mock
     InstitutionRepository institutionRepository;
 
     @Test
@@ -38,7 +41,7 @@ class InstitutionConnectorImplTest {
     }
 
     @Test
-    void save(){
+    void save() {
         Institution institution = new Institution();
         institution.setExternalId("ext");
         institution.setId("507f1f77bcf86cd799439011");
@@ -50,32 +53,74 @@ class InstitutionConnectorImplTest {
     }
 
     @Test
+    void save3() {
+        Institution institution = new Institution();
+        institution.setExternalId("ext");
+        InstitutionEntity institutionEntity = new InstitutionEntity();
+        institutionEntity.setExternalId("ext");
+        when(institutionRepository.save(any())).thenReturn(institutionEntity);
+        Institution response = institutionConnectionImpl.save(institution);
+        Assertions.assertEquals("ext", response.getExternalId());
+    }
+
+    @Test
     void findByExternalIdTest() {
         InstitutionEntity institutionEntity = new InstitutionEntity();
         institutionEntity.setId("507f1f77bcf86cd799439011");
-        when(institutionRepository.findAll((Example<InstitutionEntity>) any())).thenReturn(List.of(institutionEntity));
+        when(institutionRepository.find(any(), any())).thenReturn(new ArrayList<>());
         Optional<Institution> response = institutionConnectionImpl.findByExternalId("ext");
-        Assertions.assertTrue(response.isPresent());
-        Assertions.assertEquals("507f1f77bcf86cd799439011", response.get().getId());
+        Assertions.assertFalse(response.isPresent());
     }
 
     @Test
     void findByExternalIdNotFoundTest() {
         InstitutionEntity institutionEntity = new InstitutionEntity();
         institutionEntity.setId("507f1f77bcf86cd799439011");
-        when(institutionRepository.findAll((Example<InstitutionEntity>) any())).thenReturn(Collections.emptyList());
         Optional<Institution> response = institutionConnectionImpl.findByExternalId("ext");
         Assertions.assertTrue(response.isEmpty());
     }
 
     @Test
-    void deleteById(){
+    void deleteById() {
         doNothing().when(institutionRepository).deleteById(any());
         Assertions.assertDoesNotThrow(() -> institutionConnectionImpl.deleteById("507f1f77bcf86cd799439011"));
     }
 
     @Test
-    void save2(){
+    void testFindById() {
+        when(institutionRepository.findById(any())).thenReturn(Optional.empty());
+        assertThrows(ResourceNotFoundException.class, () -> institutionConnectionImpl.findById("42"));
+    }
+
+    @Test
+    void testFindById2() {
+        InstitutionEntity institutionEntity = new InstitutionEntity();
+        when(institutionRepository.findById(any())).thenReturn(Optional.of(institutionEntity));
+        assertNotNull(institutionConnectionImpl.findById("id"));
+    }
+    @Test
+    void testFindAndUpdate() {
+        InstitutionEntity institutionEntity = new InstitutionEntity();
+        when(institutionRepository.findAndModify(any(),any(),any(),any())).thenReturn(institutionEntity);
+        Institution response = institutionConnectionImpl.findAndUpdate("institutionId",new Onboarding(), new ArrayList<>());
+        assertNotNull(response);
+    }
+
+    @Test
+    void testFindWithFilter() {
+        when(institutionRepository.find(any(),any())).thenReturn(new ArrayList<>());
+        List<Institution> response = institutionConnectionImpl.findWithFilter("externalId","productId",new ArrayList<>());
+        assertNotNull(response);
+    }
+
+    @Test
+    void testFindInstitutionProduct() {
+        when(institutionRepository.find(any(),any())).thenReturn(new ArrayList<>());
+        assertThrows(ResourceNotFoundException.class, () -> institutionConnectionImpl.findInstitutionProduct("externalId","productId"));
+    }
+
+    @Test
+    void save2() {
         Institution institution = new Institution();
         institution.setExternalId("ext");
         institution.setId("507f1f77bcf86cd799439011");
--- connector/dao/src/test/java/it/pagopa/selfcare/mscore/connector/dao/TokenConnectorImplTest.java
+++ connector/dao/src/test/java/it/pagopa/selfcare/mscore/connector/dao/TokenConnectorImplTest.java
@@ -1,6 +1,8 @@
 package it.pagopa.selfcare.mscore.connector.dao;
 
 import it.pagopa.selfcare.mscore.connector.dao.model.TokenEntity;
+import it.pagopa.selfcare.mscore.exception.ResourceNotFoundException;
+import it.pagopa.selfcare.mscore.model.RelationshipState;
 import it.pagopa.selfcare.mscore.model.Token;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,11 +11,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 class TokenConnectorImplTest {
@@ -25,23 +30,23 @@ class TokenConnectorImplTest {
     TokenRepository tokenRepository;
 
     @Test
-    void deleteById(){
+    void deleteById() {
         doNothing().when(tokenRepository).deleteById(any());
         Assertions.assertDoesNotThrow(() -> tokenConnectorImpl.deleteById("507f1f77bcf86cd799439011"));
     }
 
     @Test
-    void findActiveContractTest(){
+    void findActiveContractTest() {
         TokenEntity token = new TokenEntity();
         token.setId("507f1f77bcf86cd799439011");
-        when(tokenRepository.find(any(),any())).thenReturn(List.of(token));
-        List<Token> tokenList = tokenConnectorImpl.findActiveContract("42","42","42");
-        Assertions.assertEquals(1,tokenList.size());
-        Assertions.assertEquals("507f1f77bcf86cd799439011",tokenList.get(0).getId());
+        when(tokenRepository.find(any(), any())).thenReturn(List.of(token));
+        List<Token> tokenList = tokenConnectorImpl.findActiveContract("42", "42", "42");
+        Assertions.assertEquals(1, tokenList.size());
+        Assertions.assertEquals("507f1f77bcf86cd799439011", tokenList.get(0).getId());
     }
 
     @Test
-    void save(){
+    void save() {
         Token token = new Token();
         token.setId("507f1f77bcf86cd799439011");
         TokenEntity tokenEntity = new TokenEntity();
@@ -50,4 +55,55 @@ class TokenConnectorImplTest {
         Token response = tokenConnectorImpl.save(token);
         Assertions.assertEquals("507f1f77bcf86cd799439011", response.getId());
     }
+
+    @Test
+    void save2() {
+        Token token = new Token();
+        token.setProductId("507f1f77bcf86cd799439011");
+        TokenEntity tokenEntity = new TokenEntity();
+        tokenEntity.setProductId("507f1f77bcf86cd799439011");
+        when(tokenRepository.save(any())).thenReturn(tokenEntity);
+        Token response = tokenConnectorImpl.save(token);
+        Assertions.assertEquals("507f1f77bcf86cd799439011", response.getProductId());
+    }
+
+    /**
+     * Method under test: {@link TokenConnectorImpl#findById(String)}
+     */
+    @Test
+    void testFindById() {
+        when(tokenRepository.findById(any())).thenReturn(Optional.empty());
+        assertThrows(ResourceNotFoundException.class, () -> tokenConnectorImpl.findById("42"));
+        verify(tokenRepository).findById(any());
+    }
+
+    @Test
+    void testFindById2() {
+        TokenEntity tokenEntity = new TokenEntity();
+        when(tokenRepository.findById(any())).thenReturn(Optional.of(tokenEntity));
+        Token response = tokenConnectorImpl.findById("tokenId");
+        assertNotNull(response);
+    }
+
+    /**
+     * Method under test: {@link TokenConnectorImpl#findAndUpdateTokenUser(String, List)}
+     */
+    @Test
+    void testFindAndUpdateTokenUser() {
+        TokenEntity tokenEntity = new TokenEntity();
+        tokenEntity.setChecksum("Checksum");
+        tokenEntity.setContract("Contract");
+        tokenEntity.setCreatedAt(null);
+        tokenEntity.setExpiringDate("2020-03-01");
+        tokenEntity.setId("42");
+        tokenEntity.setInstitutionId("42");
+        tokenEntity.setProductId("42");
+        tokenEntity.setStatus(RelationshipState.PENDING);
+        tokenEntity.setUpdatedAt(null);
+        tokenEntity.setUsers(new ArrayList<>());
+        when(tokenRepository.findAndModify(any(), any(), any(),any())).thenReturn(tokenEntity);
+        tokenConnectorImpl.findAndUpdateTokenUser("42", new ArrayList<>());
+        verify(tokenRepository).findAndModify(any(), any(), any(), any());
+    }
+
 }
