56ef59b471bbfb9c572d5678cabafddfbef0139d
Simone Martelli <simone.martelli@emea.nttdata.com>
2023-03-17 10:50:02 +0100

    retry deploy

--- connector/dao/src/main/java/it/pagopa/selfcare/mscore/connector/dao/TokenRepository.java
+++ connector/dao/src/main/java/it/pagopa/selfcare/mscore/connector/dao/TokenRepository.java
@@ -3,5 +3,6 @@ package it.pagopa.selfcare.mscore.connector.dao;
 import it.pagopa.selfcare.mscore.connector.dao.model.TokenEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TokenRepository extends MongoRepository<TokenEntity, String>, MongoCustomConnector{
+public interface TokenRepository extends MongoRepository<TokenEntity, String>, MongoCustomConnector {
+
 }
