0c68aafeef39ec47c593cc12dc8731224d20a129
SalvatorManna <117901709+SalvatorManna@users.noreply.github.com>
2023-04-21 14:37:29 +0200

    Feature/SELC 2206 (#25)

--- core/src/main/java/it/pagopa/selfcare/mscore/core/ContractService.java
+++ core/src/main/java/it/pagopa/selfcare/mscore/core/ContractService.java
@@ -229,6 +229,7 @@ public class ContractService {
     public void sendDataLakeNotification(Institution institution, Token token) {
         if (institution != null) {
             NotificationToSend notification = toNotificationToSend(institution, token);
+            log.debug("Notification to send to the data lake notification: {}", notification);
             try {
                 String msg = mapper.writeValueAsString(notification);
                 sendNotification(msg, token.getId());
@@ -270,6 +271,7 @@ public class ContractService {
         toNotify.setTaxCode(institution.getTaxCode());
         toNotify.setOrigin(institution.getOrigin());
         toNotify.setOriginId(institution.getOriginId());
+        toNotify.setZipCode(institution.getZipCode());
         return toNotify;
     }
 
