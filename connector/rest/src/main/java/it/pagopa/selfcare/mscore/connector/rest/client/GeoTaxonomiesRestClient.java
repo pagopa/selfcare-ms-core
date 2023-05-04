1a034be69af6cee10466a9964396fbb9e43002bf
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2023-05-04 17:24:12 +0200

    implemented api key for geotaxonomy

--- connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/client/GeoTaxonomiesRestClient.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/client/GeoTaxonomiesRestClient.java
@@ -1,5 +1,6 @@
 package it.pagopa.selfcare.mscore.connector.rest.client;
 
+import it.pagopa.selfcare.mscore.connector.rest.config.GeoTaxonomiesFeignClientConfig;
 import it.pagopa.selfcare.mscore.connector.rest.model.geotaxonomy.GeographicTaxonomiesResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@FeignClient(name = "${rest-client.geo-taxonomies.serviceCode}", url = "${rest-client.geo-taxonomies.base-url}")
+@FeignClient(name = "${rest-client.geo-taxonomies.serviceCode}", url = "${rest-client.geo-taxonomies.base-url}", configuration = GeoTaxonomiesFeignClientConfig.class)
 public interface GeoTaxonomiesRestClient {
 
     @GetMapping(value = "${rest-client.geo-taxonomies.getByCode.path}", consumes = APPLICATION_JSON_VALUE)
