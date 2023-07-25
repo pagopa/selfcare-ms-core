257d5896b2f9d0dbe3b6e11680d89fa24ca6430e
manuraf <manuel.rafeli@pagopa.it>
2023-07-25 15:49:57 +0200

    added endpoint controller

--- core/src/main/java/it/pagopa/selfcare/mscore/core/DelegationService.java
+++ core/src/main/java/it/pagopa/selfcare/mscore/core/DelegationService.java
@@ -2,7 +2,11 @@ package it.pagopa.selfcare.mscore.core;
 
 import it.pagopa.selfcare.mscore.model.delegation.Delegation;
 
+import java.util.List;
+
 public interface DelegationService {
 
     Delegation createDelegation(Delegation delegation);
+
+    List<Delegation> getDelegations(String from);
 }
