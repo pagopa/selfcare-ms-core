c7872ba91d1847ee1fc228940b815a0974157049
pierpaolodidato89 <pierpaolo.didato@emeal.nttdata.com>
2023-07-27 15:06:41 +0200

    Added partner's name into response of getDelegations api

--- web/src/main/java/it/pagopa/selfcare/mscore/web/controller/DelegationController.java
+++ web/src/main/java/it/pagopa/selfcare/mscore/web/controller/DelegationController.java
@@ -71,7 +71,7 @@ public class DelegationController {
     public ResponseEntity<List<DelegationResponse>> getDelegations(@ApiParam("${swagger.mscore.institutions.model.institutionId}")
                                                                    @RequestParam(name = "from", required = false) String from,
                                                                    @ApiParam("${swagger.mscore.institutions.model.institutionId}")
-                                                                    @RequestParam(name = "to", required = false) String to,
+                                                                   @RequestParam(name = "to", required = false) String to,
                                                                    @ApiParam("${swagger.mscore.product.model.id}")
                                                                    @RequestParam(name = "productId", required = false) String productId) {
 
