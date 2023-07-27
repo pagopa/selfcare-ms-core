c642d42b08626645e3815da7572caaad1ecd57d3
manuraf <manuel.rafeli@pagopa.it>
2023-07-27 14:57:14 +0200

    feat: added feature flag for sending events on ScUsers topic

---
+++ core/src/main/java/it/pagopa/selfcare/mscore/core/UserEventServiceIgnore.java
@@ -0,0 +1,25 @@
+package it.pagopa.selfcare.mscore.core;
+
+import it.pagopa.selfcare.mscore.model.onboarding.Token;
+import it.pagopa.selfcare.mscore.model.user.RelationshipInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(
+        value="core.user-event-service.type",
+        havingValue = "ignore",
+        matchIfMissing = true)
+public class UserEventServiceIgnore implements UserEventService{
+    @Override
+    public void sendLegalTokenUserNotification(Token token) {
+
+    }
+
+    @Override
+    public void sendOperatorUserNotification(RelationshipInfo relationshipInfo) {
+
+    }
+}
