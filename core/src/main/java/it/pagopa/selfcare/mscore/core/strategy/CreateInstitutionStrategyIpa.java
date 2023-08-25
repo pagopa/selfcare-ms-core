09aa376d30c77c9780502e1904be0a45effd3a4c
Manuel Rafeli <manuel.rafeli@pagopa.it>
2023-08-25 11:05:00 +0200

    feat: remove set subunitype for EC when saving institution from ip (#193)

--- core/src/main/java/it/pagopa/selfcare/mscore/core/strategy/CreateInstitutionStrategyIpa.java
+++ core/src/main/java/it/pagopa/selfcare/mscore/core/strategy/CreateInstitutionStrategyIpa.java
@@ -60,7 +60,7 @@ public class CreateInstitutionStrategyIpa implements CreateInstitutionStrategy {
                 throw new MsCoreException(CREATE_INSTITUTION_ERROR.getMessage(), CREATE_INSTITUTION_ERROR.getCode());
             }
         } else {
-           if(Objects.isNull(subunitType) || InstitutionPaSubunitType.EC.equals(subunitType)){
+           if(Objects.isNull(subunitType)){
                throw new ResourceConflictException(String
                        .format(CustomError.CREATE_INSTITUTION_CONFLICT.getMessage(), strategyInput.getTaxCode()),
                        CustomError.CREATE_INSTITUTION_CONFLICT.getCode());
@@ -91,7 +91,6 @@ public class CreateInstitutionStrategyIpa implements CreateInstitutionStrategy {
     private Institution getInstitutionEC(String taxCode, InstitutionProxyInfo institutionProxyInfo, CategoryProxyInfo categoryProxyInfo) {
 
         Institution newInstitution = institutionMapper.fromInstitutionProxyInfo(institutionProxyInfo);
-        newInstitution.setSubunitType(InstitutionPaSubunitType.EC.name());
         newInstitution.setExternalId(taxCode);
         newInstitution.setOrigin(Origin.IPA.getValue());
         newInstitution.setCreatedAt(OffsetDateTime.now());
