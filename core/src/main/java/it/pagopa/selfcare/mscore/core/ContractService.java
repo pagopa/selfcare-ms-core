b2a93c10925baa88f0412bb0d4bf4602ee54350d
bgalgamu <kevin.si1396@gmail.com>
2023-09-08 09:32:45 +0200

    test using Paths.getFile name to extrapolate fileName from contractSigned

--- core/src/main/java/it/pagopa/selfcare/mscore/core/ContractService.java
+++ core/src/main/java/it/pagopa/selfcare/mscore/core/ContractService.java
@@ -271,7 +271,8 @@ public class ContractService {
 
         // ADD or UPDATE msg event
         notification.setNotificationType(queueEvent);
-        notification.setFileName(token.getContractSigned() == null? "":  token.getContractSigned().substring(50));
+//        notification.setFileName(token.getContractSigned() == null? "":  token.getContractSigned().substring(50));
+        notification.setFileName(token.getContractSigned() == null? "":  Paths.get(token.getContractSigned()).getFileName().toString());
         notification.setContentType(token.getContentType() == null ? MediaType.APPLICATION_OCTET_STREAM_VALUE : token.getContentType());
 
         if (token.getProductId() != null && institution.getOnboarding() != null) {
