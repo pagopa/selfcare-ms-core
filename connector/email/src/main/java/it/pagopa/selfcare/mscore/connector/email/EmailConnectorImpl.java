f18b822603b90669c8157f7fc22a098361222870
pierpaolodidato89 <137791912+pierpaolodidato89@users.noreply.github.com>
2023-09-13 11:53:52 +0200

    Release UAT (#228)
    * [SELC - 3013] zip archive compression of the pdf file of the contract (#224)
    
    * fix: removed close() method and added finish() one
    
    * Fix find institution in send delegation mail (#225)
    
    * Fix find institution in sending delegation mail
    
    * Updates
    
    * Fix after pr comments
    
    * Added filter on product's status to retrieve correct pt (#227)
    
    ---------
    
    Co-authored-by: gianmarcoplutino <119858159+gianmarcoplutino@users.noreply.github.com>
    Co-authored-by: gianmarcoplutino <gianmarco.plutino@pagopa.it>

--- connector/email/src/main/java/it/pagopa/selfcare/mscore/connector/email/EmailConnectorImpl.java
+++ connector/email/src/main/java/it/pagopa/selfcare/mscore/connector/email/EmailConnectorImpl.java
@@ -2,8 +2,8 @@ package it.pagopa.selfcare.mscore.connector.email;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.selfcare.mscore.api.EmailConnector;
-import it.pagopa.selfcare.mscore.config.CoreConfig;
 import it.pagopa.selfcare.mscore.api.FileStorageConnector;
+import it.pagopa.selfcare.mscore.config.CoreConfig;
 import it.pagopa.selfcare.mscore.exception.MsCoreException;
 import it.pagopa.selfcare.mscore.model.onboarding.MailTemplate;
 import lombok.extern.slf4j.Slf4j;
@@ -12,13 +12,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.util.StringUtils;
 
+import javax.activation.DataSource;
 import javax.mail.internet.MimeMessage;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
+import static it.pagopa.selfcare.mscore.constant.GenericError.ERROR_DURING_COMPRESS_FILE;
 import static it.pagopa.selfcare.mscore.constant.GenericError.ERROR_DURING_SEND_MAIL;
 
 @Slf4j
@@ -47,7 +55,6 @@ public class EmailConnectorImpl implements EmailConnector {
             log.info("START - sendMail to {}, for product {}", destinationMail, productName);
             String template = fileStorageConnector.getTemplateFile(templateName);
             MailTemplate mailTemplate = mapper.readValue(template, MailTemplate.class);
-
             String html = StringSubstitutor.replace(mailTemplate.getBody(), mailParameters);
             log.trace("sendMessage start");
             MimeMessage mimeMessage = mailSender.createMimeMessage();
@@ -59,7 +66,9 @@ public class EmailConnectorImpl implements EmailConnector {
             message.setTo(destinationMail.toArray(new String[0]));
             message.setText(html, true);
             if(pdf != null && StringUtils.hasText(fileName)) {
-                message.addAttachment(fileName, pdf);
+                byte[] bytes = zipBytes(fileName, pdf);
+                DataSource source = new ByteArrayDataSource(bytes, "application/zip");
+                message.addAttachment(fileName + ".zip", source);
                 log.info("sendMail to: {}, attached file: {}, for product {}", destinationMail, pdf.getName(), productName);
             }
             mailSender.send(mimeMessage);
@@ -70,5 +79,21 @@ public class EmailConnectorImpl implements EmailConnector {
         }
         log.trace("sendMessage end");
     }
+    public byte[] zipBytes(String filename, File pdf)  {
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                 ZipOutputStream zos = new ZipOutputStream(baos)) {
+                ZipEntry entry = new ZipEntry(filename);
+                byte[] pdfToByte = FileCopyUtils.copyToByteArray(pdf);
+                zos.putNextEntry(entry);
+                zos.write(pdfToByte);
+                zos.closeEntry();
+                zos.finish();
+                return baos.toByteArray();
+            } catch (IOException e) {
+                log.error(String.format(ERROR_DURING_COMPRESS_FILE.getMessage(), filename), e);
+                throw new MsCoreException(String.format(ERROR_DURING_COMPRESS_FILE.getMessage(), filename),
+                        ERROR_DURING_COMPRESS_FILE.getCode());
+            }
+        }
 }
 
