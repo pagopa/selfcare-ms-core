a4c535199d356411fa96840babb2c4ced7dfc962
Andrea Grillo <andrea.grillo@pagopa.it>
2024-02-09 16:23:13 +0100

    [EC-129] Move GitHub repository settings to common module (#402)

--- .identity/env/uat/backend.tfvars
+++
@@ -1,4 +0,0 @@
-resource_group_name  = "terraform-state-rg"
-storage_account_name = "tfinfuatselfcare"
-container_name       = "terraform-state"
-key                  = "selfcare-core.identity.tfstate"
--- .identity/github_repo_variables.tf
+++
@@ -1,6 +0,0 @@
-resource "github_actions_variable" "repo_variables" {
-  for_each      = local.repo_variables
-  repository    = local.github.repository
-  variable_name = each.key
-  value         = each.value
-}
