6275967306cf5481976fa66a1620ce50a5ab875b
empassaro <emanuele.passaro@emeal.nttdata.com>
2024-02-20 14:23:37 +0100

    feature: moved queryParameters in a request body object

---
+++ web/src/main/java/it/pagopa/selfcare/mscore/web/model/institution/CreatedAtRequest.java
@@ -0,0 +1,21 @@
+package it.pagopa.selfcare.mscore.web.model.institution;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.OffsetDateTime;
+
+@Data
+public class CreatedAtRequest {
+    @NotBlank(message = "institutionId is mandatory")
+    private String institutionId;
+    @NotBlank(message = "productId is mandatory")
+    private String productId;
+    @NotNull(message = "createdAt is mandatory")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime createdAt;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime activatedAt;
+}
