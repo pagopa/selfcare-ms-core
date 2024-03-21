aec8687928bdb1adfa41a48e4364af7a108a08ff
Giulia Tremolada <giulia.tremolada@nttdata.com>
2024-03-21 12:08:04 +0100

    fix: modify delegation's findByToAndStatus to return a List

--- connector/dao/src/test/java/it/pagopa/selfcare/mscore/connector/dao/DelegationConnectorImplTest.java
+++ connector/dao/src/test/java/it/pagopa/selfcare/mscore/connector/dao/DelegationConnectorImplTest.java
@@ -27,7 +27,6 @@ import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -219,7 +218,7 @@ class DelegationConnectorImplTest {
 
     @Test
     void checkIfDelegationsAreActive() {
-        when(delegationRepository.findByToAndStatus(anyString(), any())).thenReturn(Optional.of(new DelegationEntity()));
+        when(delegationRepository.findByToAndStatus(anyString(), any())).thenReturn(Optional.of(List.of(new DelegationEntity())));
         boolean response = delegationConnectorImpl.checkIfDelegationsAreActive("id");
         assertTrue(response);
     }
