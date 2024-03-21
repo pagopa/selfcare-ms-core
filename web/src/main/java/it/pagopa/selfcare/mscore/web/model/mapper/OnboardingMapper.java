ceaae9af3df394065ec17c1f034dd74648d033a0
Manuel Rafeli <manuel.rafeli@pagopa.it>
2024-03-21 15:22:04 +0100

    hotfix: added default product role on onboardingInfo (#450)

--- web/src/main/java/it/pagopa/selfcare/mscore/web/model/mapper/OnboardingMapper.java
+++ web/src/main/java/it/pagopa/selfcare/mscore/web/model/mapper/OnboardingMapper.java
@@ -19,6 +19,7 @@ import java.util.stream.Collectors;
 @NoArgsConstructor(access = AccessLevel.NONE)
 public class OnboardingMapper {
 
+    public static final String DEFAULT_PRODUCT_ROLE = "admin";
     private static UserMapper userMapper = new UserMapperImpl();
 
     private static Contract toContract(ContractRequest request) {
@@ -92,7 +93,7 @@ public class OnboardingMapper {
             institutionResponse.setAttributes(InstitutionMapperCustom.toAttributeResponse(institution.getAttributes()));
         }
         ProductInfo productInfo = new ProductInfo();
-        productInfo.setRole(product.getProductRole());
+        productInfo.setRole(Objects.nonNull(product.getProductRole()) ? product.getProductRole() : DEFAULT_PRODUCT_ROLE);
         productInfo.setId(product.getProductId());
         productInfo.setCreatedAt(product.getCreatedAt());
         if(Objects.nonNull(product.getStatus())){
