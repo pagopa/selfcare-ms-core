3c85a53839c0f55b1ef104a0a235012267124d1d
manuraf <manuel.rafeli@pagopa.it>
2024-03-29 09:16:29 +0100

    refer to main

--- infra/container_apps/main.tf
+++ infra/container_apps/main.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 }
 
 module "container_app_core" {
-  source = "github.com/pagopa/selfcare-commons//infra/terraform-modules/container_app_microservice?ref=workload-profiles"
+  source = "github.com/pagopa/selfcare-commons//infra/terraform-modules/container_app_microservice?ref=main"
 
   is_pnpg = var.is_pnpg
 
