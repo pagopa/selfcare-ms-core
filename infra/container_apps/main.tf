ee6c4a5baefb0a7956cd780d1f4e669ccbe65d06
manuraf <manuel.rafeli@pagopa.it>
2024-04-09 15:39:03 +0200

    workload_profile_name var

--- infra/container_apps/main.tf
+++ infra/container_apps/main.tf
@@ -20,6 +20,7 @@ module "container_app_core" {
   image_tag          = var.image_tag
   app_settings       = var.app_settings
   secrets_names      = var.secrets_names
+  workload_profile_name   = var.workload_profile_name
 
   tags = var.tags
 }
