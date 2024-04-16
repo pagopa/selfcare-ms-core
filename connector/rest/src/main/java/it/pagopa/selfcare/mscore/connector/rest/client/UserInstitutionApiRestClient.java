ffada40afa0576a480b3ca082bae5f24d081b270
Kevin Silva <kevin.si1396@gmail.com>
2024-04-16 12:37:24 +0200

    removed unneccessary classes

--- connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/client/UserInstitutionApiRestClient.java
+++
@@ -1,8 +0,0 @@
-package it.pagopa.selfcare.mscore.connector.rest.client;
-
-import it.pagopa.selfcare.user.generated.openapi.v1.api.InstitutionControllerApi;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "${rest-client.user-ms.institution.serviceCode}", url = "${rest-client.user-ms.base-url}")
-public interface UserInstitutionApiRestClient extends InstitutionControllerApi {
-}
--- connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/client/UserPermissionRestClient.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.selfcare.mscore.connector.rest.client;
-
-import it.pagopa.selfcare.user.generated.openapi.v1.api.UserPermissionControllerApi;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "${rest-client.user-permission.serviceCode}", url = "${rest-client.user-ms.base-url}")
-public interface UserPermissionRestClient extends UserPermissionControllerApi {
-
-}
