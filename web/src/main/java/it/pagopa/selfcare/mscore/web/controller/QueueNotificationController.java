b672e5b8daf64763fa844cf25a10261862a467b4
Manuel Rafeli <manuel.rafeli@pagopa.it>
2024-06-07 16:34:34 +0200

    [SELC-5044] chore: remove tag external-v2 and support from /v2/delegations (#507)

--- web/src/main/java/it/pagopa/selfcare/mscore/web/controller/QueueNotificationController.java
+++ web/src/main/java/it/pagopa/selfcare/mscore/web/controller/QueueNotificationController.java
@@ -7,13 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
 @RestController
 @Slf4j
 @RequestMapping("/notification-event")
-@Api(tags = "kafka")
+@Api(tags = "Event")
 public class QueueNotificationController {
 
     private final QueueNotificationService queueNotificationService;
